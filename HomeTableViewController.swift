4e24a7584533ebceb83643e1d405e0b5f9349da5
Christopher Yecora <cey21b@my.fsu.edu>
2021-10-04 13:47:33 -0400

    Bug fixes

--- HomeTableViewController.swift
+++ HomeTableViewController.swift
@@ -18,10 +18,17 @@ class HomeTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadTweets()
+        
         
         myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
         tableView.refreshControl = myRefreshControl
+        self.tableView.rowHeight = UITableView.automaticDimension
+        self.tableView.estimatedRowHeight = 150
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        loadTweets()
     }
     
     @objc func loadTweets(){
@@ -96,8 +103,12 @@ class HomeTableViewController: UITableViewController {
             cell.proflieImageView.image = UIImage(data: imageData)
         }
         
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        
         return cell
     }
+    
+    
     
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
